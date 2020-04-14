6dbcb981dab6c7048a495cd0b5a4798df075d556
memolop10 <inglopezvargas@gmail.com>
2020-04-14 17:57:29 -0500

    usando variables de entorno

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI ?  process.env.MONGODB_URI : 'mongodb://localhost/databasetest';
 
 mongoose.connect(URI,{
     useNewUrlParser:true,
