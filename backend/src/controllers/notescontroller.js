126ae8c7482ed1f513e30ece8006ede831a47b31
memolop10 <inglopezvargas@gmail.com>
2020-05-01 18:38:33 -0500

    metodos de get y created Users implementados

---
+++ backend/src/controllers/notescontroller.js
@@ -0,0 +1,42 @@
+const notesCtrl = {};
+
+const Note = require('../models/Note');
+
+notesCtrl.getNotes = async(req, res) => {
+    const notes = await Note.find();
+    res.json(notes)
+}
+
+notesCtrl.createNote = async(req , res) =>{
+    const { title, content, date , author } = req.body;
+    const newNote = new Note({
+        title:title,
+        content:content,
+        date:date,
+        author:author
+    })
+    await newNote.save();
+    res.json({message :'Nota guardada'})
+}
+
+notesCtrl.getNote = async(req, res) =>{
+    const note = await Note.findById(req.params.id)
+    res.json(note)
+}
+
+notesCtrl.updateNote = async(req, res) => {
+    const { title,author, content } = req.body;
+    await Note.findOneAndUpdate({_id: req.params.id},{
+        title,
+        author,
+        content
+    })
+    res.json({message:'Note update'})
+}
+
+notesCtrl.deleteNote= async(req , res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({message: 'Nota eliminada'})
+}
+
+module.exports = notesCtrl;
---
+++ backend/src/controllers/userscontroller.js
@@ -0,0 +1,19 @@
+const userCtrl = {}; 
+
+const User = require('../models/User'); 
+
+userCtrl.getUsers = async(req, res) =>{
+    const users = await User.find();
+    res.json(users)
+} 
+
+userCtrl.createUser = async(req, res) => {
+    const {username} = req.body;
+    const newUser = new User({username});
+    await newUser.save()
+    res.json('user created') 
+}
+
+userCtrl.deleteUser = (req, res) => res.json({message: 'user borrado'})
+
+module.exports = userCtrl;
