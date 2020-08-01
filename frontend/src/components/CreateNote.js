a0c999baa636a4092cd525caa3a73d0da704be18
memolop10 <inglopezvargas@gmail.com>
2020-08-01 14:35:48 -0500

    component Notelist avanced

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -8,20 +8,31 @@ export default class CreateNote extends Component {
     state={
         users:[],
         userSelected:'',
-        tittle:'',
+        title:'',
         content:'',
         date: new Date()
     }
 
     async componentDidMount(){
        const res = await axios.get('http://localhost:4000/api/users');
-       this.setState({users: res.data.map(user => user.username)})
+            this.setState({users: res.data.map(user => user.username),
+            userSelected:res.data[0].username
+        })
     }
 
-    onSubmit = (e) => {
-       e.preventDefault();
-    }
+    onSubmit = async (e) => {
+        e.preventDefault();
+    
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        }
+            await axios.post('http://localhost:4000/api/notes', newNote);
 
+            window.location.href = '/'
+    }
     onInputChange= e =>{
         this.setState({
             [e.target.name]: e.target.value
@@ -58,8 +69,8 @@ export default class CreateNote extends Component {
                     <input 
                         type="text"
                         className="form-control"
-                        placeholder="Tittle"
-                        name="tittle"
+                        placeholder="Title"
+                        name="title"
                         onChange={this.onInputChange}
                         required/>
                 </div>
